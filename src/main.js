7b943b85d382dea732d2d67de53f6732c439c4d4
S-Rashy <sadiqrashidah@gmail.com>
2026-01-31 23:32:06 +0100

    Added Cart store

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { store } from './stores'
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(pinia)
 
 app.mount('#app')
